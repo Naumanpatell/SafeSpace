b27570c4fb965304e0ed43af67e20616a8ee17cf
Naumanpatell <nauman0504@gmail.com>
2026-03-09 14:58:13 +0000

    changed the training code to make the training better

--- model/train.py
+++ model/train.py
@@ -1,43 +1,55 @@
 import pandas as pd
 from datasets import Dataset
-from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from transformers import DistilBertTokenizer
+from transformers import DistilBertForSequenceClassification
 from transformers import TrainingArguments, Trainer
+
+print("Loading dataset...")
 
 df = pd.read_csv("dataset.csv")
 
 dataset = Dataset.from_pandas(df)
 
-model_name = "distilbert-base-uncased"
-
-tokenizer = AutoTokenizer.from_pretrained(model_name)
+tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased")
 
 def tokenize(example):
-    return tokenizer(example["text"], truncation=True, padding="max_length")
+    return tokenizer(example["text"], truncation=True, padding="max_length", max_length=128)
 
-dataset = dataset.map(tokenize, batched=True)
+dataset = dataset.map(tokenize)
 
 dataset = dataset.train_test_split(test_size=0.1)
 
-model = AutoModelForSequenceClassification.from_pretrained(
-    model_name,
-    num_labels=2
+train_dataset = dataset["train"]
+test_dataset = dataset["test"]
+
+model = DistilBertForSequenceClassification.from_pretrained(
+    "distilbert-base-uncased",
+    num_labels=4
 )
 
 training_args = TrainingArguments(
     output_dir="./results",
-    learning_rate=2e-5,
-    per_device_train_batch_size=4,
-    num_train_epochs=1
+    num_train_epochs=1,
+    per_device_train_batch_size=16,
+    logging_steps=50,
+    save_steps=500,
+    max_steps=1000
 )
 
 trainer = Trainer(
     model=model,
     args=training_args,
-    train_dataset=dataset["train"],
-    eval_dataset=dataset["test"]
+    train_dataset=train_dataset,  
+    eval_dataset=test_dataset
 )
+
+print("Starting training...")
 
 trainer.train()
 
-trainer.save_model("saved_model")
+print("Saving model...")
+
+model.save_pretrained("saved_model")
 tokenizer.save_pretrained("saved_model")
+
+print("Training complete")
